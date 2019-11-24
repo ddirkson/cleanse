49faadf7de160c797266408bbc1269cc75682114
SodakDoubleD <sodakdoubled@gmail.com>
2019-11-23 22:56:21 -0600

    stub out main and arg parsing

--- cleanse.py
+++ cleanse.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python3
 
+import argparse
+
+
+def main():
+    pass
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    arguments = parser.parse_args()
+    return arguments
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(*args)
