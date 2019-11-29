1155a72883707593526b7bfb533446f7cea5a884
SodakDoubleD <sodakdoubled@gmail.com>
2019-11-28 22:27:00 -0600

    add first test and relevant test files

--- cleanse/tests/test_cleanse.py
+++ cleanse/tests/test_cleanse.py
@@ -1,7 +1,22 @@
 
-import cleanse
+import os
+import pytest
+from cleanse import cleanse
 
 
-def test_traverse_nodes():
-    xml_string = '<root><test>blah</test></root>'
-    cleanse.traverse_nodes(xml_string)
+def test_end_to_end():
+    file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test.xml')
+    mapping_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mapping.json')
+    assert os.path.exists(file_path)
+    assert os.path.exists(mapping_file)
+
+    pre_data = ''
+    with open(file_path, 'r') as file:
+        pre_data = file.read()
+
+    cleanse.cleanse_xml(file_path, mapping_file)
+    post_data = ''
+    with open(file_path, 'r') as file:
+        post_data = file.read()
+
+    assert pre_data != post_data
